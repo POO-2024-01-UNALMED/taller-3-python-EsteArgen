0a5263b333b18af38961df6e79207498154b2d27
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-01-18 18:29:47 -0500

    Prueba Dos

---
+++ televisores/marca.py
@@ -0,0 +1,9 @@
+class Marca:
+    def __init__(self, nombre: str) -> None:
+        self._nombre = nombre
+
+    def getNombre(self) -> str:
+        return self._nombre
+
+    def setNombre(self, nombre: str) -> None:
+        self._nombre = nombre
